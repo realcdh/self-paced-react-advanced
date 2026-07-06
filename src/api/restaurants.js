60bad9427314eadf514bf620649b42f77071bd49
realcdh <rdhdevelop@gmail.com>
2026-07-06 11:34:16 +0900

    refactor: API URL 상수화 및 RestaurantContext 파일 삭제

--- src/api/restaurants.js
+++ src/api/restaurants.js
@@ -1,13 +1,13 @@
-const RESTAURANTS_API_URL = "http://localhost:3000/restaurants";
+const baseUrl = "http://localhost:3000";
 
 export const fetchRestaurants = async () => {
-  const response = await fetch(RESTAURANTS_API_URL);
+  const response = await fetch(`${baseUrl}/restaurants`);
   if (!response.ok) throw new Error("음식점 목록을 불러오지 못했어요.");
   return response.json();
 };
 
 export const postRestaurant = async (newRestaurant) => {
-  const response = await fetch(RESTAURANTS_API_URL, {
+  const response = await fetch(`${baseUrl}/restaurants`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(newRestaurant),
